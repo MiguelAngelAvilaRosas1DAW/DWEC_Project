cc0de1dafa83c4dcc1683c48563f8246f44abf3e
Miguel Ángel Ávila Rosas <148963870+MiguelAngelAvilaRosas1DAW@users.noreply.github.com>
2024-09-20 13:05:09 +0200

    Delete Plantilla03_Ejercicio1 directory

--- Plantilla03_Ejercicio1/ejercicios/js/funciones.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-/*Obtains the 'name' atribute from the label 'resultado and changes the text'.*/
-function mostrar(resultado) {
-    console.log(`Estoy mostrando el resultado del ${resultado.getAttribute('name')}`);
-    resultado.innerHTML = `Estoy mostrando el resultado del ${resultado.getAttribute('name')}`;
-}
--- Plantilla03_Ejercicio1/ejercicios/js/script.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-/*Gets the labels with the ID 'boton' and 'resultado' and when the button 'boton' is pressed,
-calls the function 'mostrar' to change the label with the ID 'resultado'.*/
-const boton = document.getElementById("boton");
-const resultado = document.getElementById("resultado");
-boton.addEventListener("click", function () {
-    mostrar(resultado);
-});
